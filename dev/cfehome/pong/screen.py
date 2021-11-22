fc56a20ab2a4abc2a259ed26945c4260f85ea61f
Anshuman-Raj <anshuman.raj10@gmail.com>
2021-11-22 15:16:22 +0530

    Air Hockey Complete

--- dev/cfehome/pong/screen.py
+++ dev/cfehome/pong/screen.py
@@ -26,32 +26,47 @@ paddle_left.update_score()
 paddle_right.update_score()
 ball = Ball()
 screen.listen()
-while paddle_left.score < 5 and paddle_right.score < 5:
-    ball.move()
-    sleep(0.10)
-    screen.onkeypress(key="Up", fun=paddle_right.move_up)
-    screen.onkeyrelease(key='Up', fun=None)
-    screen.onkeypress(key="Down", fun=paddle_right.move_down)
-    screen.onkeyrelease(key='Down', fun=None)
-    screen.onkeypress(key="w", fun=paddle_left.move_up)
-    screen.onkeyrelease(key='w', fun=None)
-    screen.onkeypress(key="s", fun=paddle_left.move_down)
-    screen.onkeyrelease(key='s', fun=None)
-    if paddle_left.distance(ball.ball) <= 20:
-        ball.collide_with_paddle()
-    if paddle_right.distance(ball.ball) <= 20:
-        ball.collide_with_paddle()
-    sleep(0.02)
-    ball.collide_with_wall()
-    ball.keep_score(paddle_left, paddle_right)
-    screen.update()
+temp = Turtle()
+temp.hideturtle()
+temp.pu()
+temp.goto(x=0, y=0)
+temp.color('Green')
+temp.write('Press space-bar to start', align='center', font=('Arial', 24, 'bold'))
+screen.update()
 
-draw_screen.pu()
-draw_screen.goto(x=0, y=0)
-if paddle_left.score == 5:
-    draw_screen.color('blue')
-    draw_screen.write('Blue Won', align='center', font=('Arial', 24, 'bold'))
-else:
-    draw_screen.color('red')
-    draw_screen.write('Red Won', align='center', font=('Arial', 24, 'bold'))
+
+def start():
+    temp.clear()
+    while paddle_left.score < 5 and paddle_right.score < 5:
+        ball.move()
+        sleep(0.10)
+        screen.onkeypress(key="Up", fun=paddle_right.move_up)
+        screen.onkeyrelease(key='Up', fun=None)
+        screen.onkeypress(key="Down", fun=paddle_right.move_down)
+        screen.onkeyrelease(key='Down', fun=None)
+        screen.onkeypress(key="w", fun=paddle_left.move_up)
+        screen.onkeyrelease(key='w', fun=None)
+        screen.onkeypress(key="s", fun=paddle_left.move_down)
+        screen.onkeyrelease(key='s', fun=None)
+        if paddle_left.distance(ball.ball) <= 20:
+            ball.collide_with_paddle()
+        if paddle_right.distance(ball.ball) <= 20:
+            ball.collide_with_paddle()
+        sleep(0.017)
+        ball.collide_with_wall()
+        ball.keep_score(paddle_left, paddle_right)
+        screen.update()
+    draw_screen.pu()
+    draw_screen.goto(x=0, y=0)
+    if paddle_left.score == 5:
+        draw_screen.color('blue')
+        draw_screen.write('Blue Won', align='center', font=('Arial', 24, 'bold'))
+    else:
+        draw_screen.color('red')
+        draw_screen.write('Red Won', align='center', font=('Arial', 24, 'bold'))
+
+
+screen.onkeypress(key='space', fun=start)
+
+
 screen.mainloop()
